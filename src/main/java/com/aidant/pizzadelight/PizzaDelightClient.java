aa78ec6d642c15696f45a448e2f8767dc3a355cb
AidanTilanus <Aidan.tilanus@gmail.com>
2024-08-31 13:48:52 +0200

    -

---
+++ src/main/java/com/aidant/pizzadelight/PizzaDelightClient.java
@@ -0,0 +1,10 @@
+package com.aidant.pizzadelight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PizzaDelightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
