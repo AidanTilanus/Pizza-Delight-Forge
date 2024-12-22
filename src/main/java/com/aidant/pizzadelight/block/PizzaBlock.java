1a01ed4694a26ae1d8b4cf1a3e5d440067bdf699
AidanTilanus <Aidan.tilanus@gmail.com>
2024-12-22 17:11:57 +0100

    Create PizzaBlock.java

---
+++ src/main/java/com/aidant/pizzadelight/block/PizzaBlock.java
@@ -0,0 +1,10 @@
+package com.aidant.pizzadelight.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class PizzaBlock extends Block {
+
+    public PizzaBlock(Properties pProperties) {
+        super(pProperties);
+    }
+}
